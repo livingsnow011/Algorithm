47b1bafa79e210454169d976ff57e4a6731cd20a
SSOL <livingsnow011@gmail.com>
2022-01-07 05:35:04 +0900

    create repo

---
+++ core/src/main/java/hello/core/member/MemberServiceImpl.java
@@ -0,0 +1,18 @@
+package hello.core.member;
+
+public class MemberServiceImpl implements MemberService{
+    //왜 final인지 기본 개념 햇갈림
+    private final MemberRepository memberRepository = new
+            MemoryMemberRepository();
+
+    @Override
+    public void join(Member member) {
+        memberRepository.save(member);
+    }
+
+    @Override
+    public Member findMember(Long memberId) {
+        return memberRepository.findById(memberId);
+    }
+
+}
