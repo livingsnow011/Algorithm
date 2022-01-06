59ac5766715afb432647be99510b3a84b8f46c75
SSOL <83905764+livingsnow011@users.noreply.github.com>
2022-01-07 05:28:10 +0900

    Delete src directory

--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-#include"solution.h"
-using namespace std;
-
-int main() {
-	c3_4();
-	return 0;
-}
--- src/solution.h
+++
@@ -1,5 +0,0 @@
-void c3_1();
-void c3_2();
-void c3_3();
-void c3_4();
-void c4_1();
