4fc1166efe032e758483b5c683e68acd89d34f1b
SSOL <livingsnow011@gmail.com>
2022-01-25 23:32:44 +0900

    delete Test

--- core/src/main/java/test/test.java
+++ core/src/main/java/test/test.java
@@ -1,14 +1,16 @@
 package test;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class test {
 
-    public static void main(String[] args) {
-        int a =10;
-        char b = 'a';
-        double c = 5.7;
-        String d = "a";
+            public static void main(String[] args) {
+                List<String> list = Arrays.asList("A", "B", "C", "A");
+                List<String> newList = list.stream().distinct().collect(Collectors.toList());
+                System.out.println(newList);
+                System.out.println(list);
+            }
 
-        String q = (char)d;
-        System.out.println(q);
-    }
 }
