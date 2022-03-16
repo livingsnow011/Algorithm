d3e3906412b674410cf009b076d1595141981edf
SOL <livingsnow011@gmail.com>
2022-03-16 22:37:10 +0900

    jpa study

---
+++ freelec-springboot2-webservice/src/test/java/com/jojoldu/book/springboot/domain/posts/PostRepositoryTest.java
@@ -0,0 +1,48 @@
+package com.jojoldu.book.springboot.domain.posts;
+
+import org.assertj.core.api.Assertions;
+import org.junit.After;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class PostRepositoryTest {
+
+    @Autowired
+    PostRepository postRepository;
+
+    @After
+    public void cleanup(){
+        postRepository.deleteAll();
+    }
+
+    @Test
+    public void 게시글저장_불러오기() {
+        //given
+        String title = "테스트 게시글";
+        String content = "테스트 본문";
+
+        postRepository.save(Posts.builder()
+                .title(title)
+                .content(content)
+                .author("sju01132@gmail.com")
+                .build());
+
+        //when
+        List<Posts> postsList = postRepository.findAll();
+
+        //then
+        Posts posts = postsList.get(0);
+        Assertions.assertThat(posts.getTitle()).isEqualTo(title);
+        Assertions.assertThat(posts.getContent()).isEqualTo(content);
+    }
+
+}
